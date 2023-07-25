c831437928e20236752cc482d19600908ce13c62
CllDMR <celal.demirx@gmail.com>
2023-07-25 10:59:20 +0300

    feat: created supplier-checkout api router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,12 +1,14 @@
 import { authRouter } from "./router/auth";
 import { customerCheckoutRouter } from "./router/customer-checkout";
 import { productRouter } from "./router/product";
+import { supplierCheckoutRouter } from "./router/supplier-checkout";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   product: productRouter,
   customerCheckout: customerCheckoutRouter,
+  supplierCheckout: supplierCheckoutRouter,
 });
 
 // export type definition of API
