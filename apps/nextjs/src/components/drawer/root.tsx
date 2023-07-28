b5d2973d04a3c609e54b03c3c2a73ed50ffdd7d5
CllDMR <celal.demirx@gmail.com>
2023-07-28 15:52:49 +0300

    feat: added page links to drawers

--- apps/nextjs/src/components/drawer/root.tsx
+++ apps/nextjs/src/components/drawer/root.tsx
@@ -10,7 +10,25 @@ export const DrawerRoot: FC = () => (
         <Link href="/dashboard">Dashboard</Link>
       </li>
       <li>
+        <Link href="/customer-checkouts">CustomerCheckouts</Link>
+      </li>
+      <li>
+        <Link href="/customer-payments">CustomerPayments</Link>
+      </li>
+      <li>
+        <Link href="/customers">Customers</Link>
+      </li>
+      <li>
         <Link href="/products">Products</Link>
+      </li>
+      <li>
+        <Link href="/supplier-checkouts">SupplierCheckouts</Link>
+      </li>
+      <li>
+        <Link href="/supplier-payments">SupplierPayments</Link>
+      </li>
+      <li>
+        <Link href="/suppliers">Suppliers</Link>
       </li>
     </ul>
   </aside>
