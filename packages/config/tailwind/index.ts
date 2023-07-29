04b81308122efc21458dcb28ce8411df774146c6
mozanyazar <mahmutozanyazar@gmail.com>
2023-07-29 23:41:08 +0300

    added variables for themes

--- packages/config/tailwind/index.ts
+++ packages/config/tailwind/index.ts
@@ -3,12 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   content: [""],
   theme: {
-    extend: {
-      colors: {
-        dark: "#f0f2f5",
-        primary: "#e32f6f",
-      },
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
@@ -17,14 +12,18 @@ export default {
         light: {
           ...require("daisyui/src/theming/themes")["[data-theme=light]"],
           "base-100": "#efefef",
+          primary: "#1a2035",
           ".sidebar": {
-            "background-image": "linear-gradient(195deg,#42424a,#191919)",
+            "background-image": "none",
+            background: "#ededed",
           },
         },
         dark: {
           ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
           "base-100": "#1a2035",
+          primary: "#ec407a",
           ".sidebar": {
+            "background-color": "#1a2035",
             "background-image": "linear-gradient(195deg,#323a54,#1a2035)",
           },
         },
