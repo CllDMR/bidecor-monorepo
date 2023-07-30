0c7df4a8603d3ae8d5343f90700486e0ff986377
CllDMR <celal.demirx@gmail.com>
2023-07-30 11:42:54 +0300

    feat: :art: renamed packages-app-root package.json names

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,12 +1,12 @@
 // Importing env files here to validate on build
+import "@bidecor/auth/env.mjs";
 import "./src/env.mjs";
-import "@acme/auth/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  transpilePackages: ["@bidecor/api", "@bidecor/auth", "@bidecor/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
