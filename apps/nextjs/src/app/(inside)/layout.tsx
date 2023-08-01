53f124264da7ea019e541cc058c583b3ed4bb765
mozanyazar <mahmutozanyazar@gmail.com>
2023-08-01 23:03:49 +0300

    root layout added, and provider moved to root layout.

--- apps/nextjs/src/app/(inside)/layout.tsx
+++ apps/nextjs/src/app/(inside)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 import "~/styles/globals.css";
 
@@ -7,12 +6,6 @@ import type { FC, PropsWithChildren, ReactNode } from "react";
 
 import { DrawerInside } from "~/components/drawer/inside";
 import { NavbarInside } from "~/components/navbar/inside";
-import { Provider } from "~/components/providers";
-
-const fontSans = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "Create T3 Turbo",
@@ -24,25 +17,17 @@ const Layout: FC<PropsWithChildren & { modal: ReactNode }> = ({
   modal,
 }) => {
   return (
-    <html lang="en">
-      <body
-        className={`${["font-sans", fontSans.variable].join(" ")} base-100`}
-      >
-        <Provider>
-          <div className="drawer lg:drawer-open">
-            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />{" "}
-            <div className="drawer-content box-border flex min-h-[100vh] flex-col p-3 sm:p-4 lg:p-6">
-              <NavbarInside />
-              <main>
-                {children}
-                {modal}
-              </main>
-            </div>
-            <DrawerInside />
-          </div>
-        </Provider>
-      </body>
-    </html>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />{" "}
+      <div className="drawer-content box-border flex min-h-[100vh] flex-col p-3 sm:p-4 lg:p-6">
+        <NavbarInside />
+        <main>
+          {children}
+          {modal}
+        </main>
+      </div>
+      <DrawerInside />
+    </div>
   );
 };
 
